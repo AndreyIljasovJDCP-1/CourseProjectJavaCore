638fce2321dddc188b8e7a69a935dab9dcd9e3b3
Andrey Iljasov <a_iljasov@mail.ru>
2022-10-12 20:55:58 +0300

    basetask tests develop

---
+++ src/test/java/statistics/StatisticTest.java
@@ -0,0 +1,29 @@
+package statistics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getRequestFromJsonString() {
+    }
+
+    @Test
+    void addToCategoryMap() {
+    }
+
+    @Test
+    void getMaxCategory() {
+    }
+
+    @Test
+    void getJsonStringFromResponse() {
+    }
+}
