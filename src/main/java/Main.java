b45ea2f15bffc752393d8874a034126de2fab2ec
Andrey Iljasov <a_iljasov@mail.ru>
2022-10-14 19:49:39 +0300

    Server 8989 testClientServer untracked

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public class Main {
 
-    private static final int PORT = 8085;
+    private static final int PORT = 8989;
 
     public static void main(String[] args) {
         File autoSave = new File("data.bin");
